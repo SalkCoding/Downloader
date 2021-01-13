30bfa40a4c350a200571197b954c837cdd10f523
Josangbeom <josangbeom1104@gmail.com>
2021-01-13 15:20:04 +0900

    All features were implemented

---
+++ TunaDownloader/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿
+namespace TunaDownloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.downloadProgressBar = new MaterialSkin.Controls.MaterialProgressBar();
+            this.materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
+            this.percentageLabel = new MaterialSkin.Controls.MaterialLabel();
+            this.startButton = new MaterialSkin.Controls.MaterialButton();
+            this.cancelButton = new MaterialSkin.Controls.MaterialButton();
+            this.folderTextBox = new MaterialSkin.Controls.MaterialTextBox();
+            this.fileNameTextBox = new MaterialSkin.Controls.MaterialTextBox();
+            this.materialLabel2 = new MaterialSkin.Controls.MaterialLabel();
+            this.materialLabel3 = new MaterialSkin.Controls.MaterialLabel();
+            this.SuspendLayout();
+            // 
+            // downloadProgressBar
+            // 
+            this.downloadProgressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.downloadProgressBar.Depth = 0;
+            this.downloadProgressBar.Location = new System.Drawing.Point(12, 248);
+            this.downloadProgressBar.MouseState = MaterialSkin.MouseState.HOVER;
+            this.downloadProgressBar.Name = "downloadProgressBar";
+            this.downloadProgressBar.Size = new System.Drawing.Size(456, 5);
+            this.downloadProgressBar.TabIndex = 0;
+            // 
+            // materialLabel1
+            // 
+            this.materialLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.materialLabel1.AutoSize = true;
+            this.materialLabel1.Depth = 0;
+            this.materialLabel1.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.materialLabel1.Location = new System.Drawing.Point(168, 216);
+            this.materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel1.Name = "materialLabel1";
+            this.materialLabel1.Size = new System.Drawing.Size(81, 19);
+            this.materialLabel1.TabIndex = 1;
+            this.materialLabel1.Text = "다운로드 진행:";
+            // 
+            // percentageLabel
+            // 
+            this.percentageLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.percentageLabel.AutoSize = true;
+            this.percentageLabel.Depth = 0;
+            this.percentageLabel.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.percentageLabel.Location = new System.Drawing.Point(255, 216);
+            this.percentageLabel.MouseState = MaterialSkin.MouseState.HOVER;
+            this.percentageLabel.Name = "percentageLabel";
+            this.percentageLabel.Size = new System.Drawing.Size(22, 19);
+            this.percentageLabel.TabIndex = 2;
+            this.percentageLabel.Text = "0%";
+            // 
+            // startButton
+            // 
+            this.startButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.startButton.AutoSize = false;
+            this.startButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.startButton.Depth = 0;
+            this.startButton.DrawShadows = true;
+            this.startButton.HighEmphasis = true;
+            this.startButton.Icon = null;
+            this.startButton.Location = new System.Drawing.Point(50, 262);
+            this.startButton.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.startButton.MouseState = MaterialSkin.MouseState.HOVER;
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(150, 25);
+            this.startButton.TabIndex = 3;
+            this.startButton.Text = "다운로드";
+            this.startButton.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.startButton.UseAccentColor = false;
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.cancelButton.AutoSize = false;
+            this.cancelButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.cancelButton.Depth = 0;
+            this.cancelButton.DrawShadows = true;
+            this.cancelButton.HighEmphasis = true;
+            this.cancelButton.Icon = null;
+            this.cancelButton.Location = new System.Drawing.Point(275, 262);
+            this.cancelButton.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.cancelButton.MouseState = MaterialSkin.MouseState.HOVER;
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(150, 25);
+            this.cancelButton.TabIndex = 4;
+            this.cancelButton.Text = "취소";
+            this.cancelButton.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.cancelButton.UseAccentColor = false;
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.CancelButton_Click);
+            // 
+            // folderTextBox
+            // 
+            this.folderTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.folderTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.folderTextBox.Depth = 0;
+            this.folderTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.folderTextBox.Location = new System.Drawing.Point(12, 100);
+            this.folderTextBox.MaxLength = 255;
+            this.folderTextBox.MouseState = MaterialSkin.MouseState.OUT;
+            this.folderTextBox.Multiline = false;
+            this.folderTextBox.Name = "folderTextBox";
+            this.folderTextBox.ReadOnly = true;
+            this.folderTextBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Horizontal;
+            this.folderTextBox.Size = new System.Drawing.Size(456, 36);
+            this.folderTextBox.TabIndex = 5;
+            this.folderTextBox.Text = "C:\\";
+            this.folderTextBox.UseTallSize = false;
+            this.folderTextBox.Click += new System.EventHandler(this.FolderTextBox_Click);
+            // 
+            // fileNameTextBox
+            // 
+            this.fileNameTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.fileNameTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.fileNameTextBox.Depth = 0;
+            this.fileNameTextBox.DetectUrls = false;
+            this.fileNameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.fileNameTextBox.Location = new System.Drawing.Point(12, 171);
+            this.fileNameTextBox.MaxLength = 50;
+            this.fileNameTextBox.MouseState = MaterialSkin.MouseState.OUT;
+            this.fileNameTextBox.Multiline = false;
+            this.fileNameTextBox.Name = "fileNameTextBox";
+            this.fileNameTextBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Horizontal;
+            this.fileNameTextBox.Size = new System.Drawing.Size(456, 36);
+            this.fileNameTextBox.TabIndex = 6;
+            this.fileNameTextBox.Text = "TunaResourcepack";
+            this.fileNameTextBox.UseTallSize = false;
+            // 
+            // materialLabel2
+            // 
+            this.materialLabel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.materialLabel2.AutoSize = true;
+            this.materialLabel2.Depth = 0;
+            this.materialLabel2.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.materialLabel2.Location = new System.Drawing.Point(12, 148);
+            this.materialLabel2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel2.Name = "materialLabel2";
+            this.materialLabel2.Size = new System.Drawing.Size(37, 19);
+            this.materialLabel2.TabIndex = 7;
+            this.materialLabel2.Text = "파일명";
+            // 
+            // materialLabel3
+            // 
+            this.materialLabel3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.materialLabel3.AutoSize = true;
+            this.materialLabel3.Depth = 0;
+            this.materialLabel3.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.materialLabel3.Location = new System.Drawing.Point(12, 77);
+            this.materialLabel3.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel3.Name = "materialLabel3";
+            this.materialLabel3.Size = new System.Drawing.Size(233, 19);
+            this.materialLabel3.TabIndex = 8;
+            this.materialLabel3.Text = "클릭 시 저장할 위치를 지정할 수 있습니다.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 300);
+            this.Controls.Add(this.materialLabel3);
+            this.Controls.Add(this.materialLabel2);
+            this.Controls.Add(this.fileNameTextBox);
+            this.Controls.Add(this.folderTextBox);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.percentageLabel);
+            this.Controls.Add(this.materialLabel1);
+            this.Controls.Add(this.downloadProgressBar);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "TunaDownloader";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialProgressBar downloadProgressBar;
+        private MaterialSkin.Controls.MaterialLabel materialLabel1;
+        private MaterialSkin.Controls.MaterialLabel percentageLabel;
+        private MaterialSkin.Controls.MaterialButton startButton;
+        private MaterialSkin.Controls.MaterialButton cancelButton;
+        private MaterialSkin.Controls.MaterialTextBox folderTextBox;
+        private MaterialSkin.Controls.MaterialTextBox fileNameTextBox;
+        private MaterialSkin.Controls.MaterialLabel materialLabel2;
+        private MaterialSkin.Controls.MaterialLabel materialLabel3;
+    }
+}
+
---
+++ TunaDownloader/Form1.cs
@@ -0,0 +1,89 @@
+﻿using MaterialSkin.Controls;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
+using System.ComponentModel;
+using System.Media;
+using System.Net;
+using System.Windows.Forms;
+
+namespace TunaDownloader
+{
+    public partial class Form1 : MaterialForm
+    {
+        private WebClient client;
+        private bool isDownloading = false;
+        private readonly Uri uri = new Uri(@"https://speed.hetzner.de/100MB.bin");
+        private readonly string defaultDirectory = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\.minecraft\resourcepacks";
+
+        public Form1()
+        {
+            InitializeComponent();
+            folderTextBox.Text = defaultDirectory;
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            if (isDownloading)
+            {
+                MessageBox.Show("이미 다운로드가 진행 중입니다!", "오류!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            string fileLocation = string.Format(@"{0}\{1}.zip", folderTextBox.Text, fileNameTextBox.Text);
+            client = new WebClient();
+            client.DownloadProgressChanged += DownloadProgressChanged;
+            client.DownloadFileCompleted += DownloadCompleted;
+            client.DownloadFileAsync(uri, fileLocation);
+            isDownloading = true;
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            if (!isDownloading)
+                return;
+
+            client.DownloadFileCompleted -= DownloadCompleted;
+            client.CancelAsync();
+            client.Dispose();
+            percentageLabel.Text = "0%";
+            downloadProgressBar.Value = 0;
+            isDownloading = false;
+            SystemSounds.Beep.Play();
+        }
+
+        private void FolderTextBox_Click(object sender, EventArgs e)
+        {
+            using (CommonOpenFileDialog dialog = new CommonOpenFileDialog())
+            {
+                dialog.InitialDirectory = defaultDirectory;
+                dialog.IsFolderPicker = true;
+                if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+                {
+                    folderTextBox.Text = dialog.FileName;
+                }
+            }
+        }
+
+        private void DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            int percentage = e.ProgressPercentage;
+            downloadProgressBar.Value = percentage;
+            percentageLabel.Text = string.Format("{0:0.##}%", (double)e.BytesReceived / (double)e.TotalBytesToReceive * 100);
+        }
+
+        private void DownloadCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            client.Dispose();
+            isDownloading = false;
+            SystemSounds.Beep.Play();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (client != null)
+            {
+                client.CancelAsync();
+                client.Dispose();
+            }
+        }
+    }
+}
